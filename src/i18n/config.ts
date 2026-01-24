f2a0509ad2e7812c9880c96aa14ea1e9481d9b0c
ramlow07 <luamramlow@gmail.com>
2026-01-24 15:25:09 -0300

    adding i18n

---
+++ src/i18n/config.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+import pt from './locales/pt.json';
+import de from './locales/de.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      pt: { translation: pt },
+      de: { translation: de },
+    },
+    lng: 'en', // default language
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
